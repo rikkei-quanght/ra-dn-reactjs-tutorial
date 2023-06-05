4b19736dce9b267f9fc393c1ce0f92f6ecec10e8
rk-quanght <quanght@rikkeisoft.com>
2023-06-05 20:57:55 +0700

    session 04 - vi du useReducer()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {useState, useReducer} from 'react'
 import './App.css';
 
 function App() {
+  // const [count, setCount] = useState(0)
+  const UP = 'UP'
+  const DOWN = 'DOWN'
+
+  const reducer = (state, action) => {
+    if (action === UP) {
+      return state + 1
+    } else if (action === DOWN) {
+      return state - 1
+    }
+  }
+
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REDUCER</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(UP)}>Up</button>
+      <button onClick={() => dispatch(DOWN)}>Down</button>
     </div>
   );
 }
