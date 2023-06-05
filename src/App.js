6bc998e9509a830124ee703fa448c0483305b4c9
rk-quanght <quanght@rikkeisoft.com>
2023-06-05 20:14:02 +0700

    session 04 - ví dụ về useState()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(2);
+
+  const handleRandomButton = () => {
+    setNumber(Math.floor(Math.random() * 10))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random number</h1>
+      <p>Number: {number}</p>
+      <button onClick={handleRandomButton}>Random</button>
     </div>
   );
 }
